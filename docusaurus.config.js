ed6127f37d9ec94f3e2d2328671a4cbc52d16271
Jayaraj156 <86654852+Jayaraj156@users.noreply.github.com>
2022-05-11 17:03:01 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
   baseUrl: '/help/',
   onBrokenLinks: 'throw',
   favicon: 'img/Logo.png',
-  organizationName: 'your-org', // Usually your GitHub org/user name.
+ organizationName: 'your-org', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
 //    algolia: {
@@ -13,41 +13,12 @@ module.exports = {
 //      indexName: 'YOUR_INDEX_NAME',
 //    },
     colorMode: {
-      // "light" | "dark"
       defaultMode: 'light',
-
-      // Hides the switch in the navbar
-      // Useful if you want to support a single color mode
       disableSwitch: true,
-
-      // Should we use the prefers-color-scheme media-query,
-      // using user system preferences, instead of the hardcoded defaultMode
       respectPrefersColorScheme: false,
-
-      // Dark/light switch icon options
-      switchConfig: {
-        // Icon for the switch while in dark mode
-        darkIcon: '🌙',
-
-        // CSS to apply to dark icon,
-        // React inline style object
-        // see https://reactjs.org/docs/dom-elements.html#style
-        darkIconStyle: {
-          marginLeft: '2px',
-        },
-
-        // Unicode icons such as '\u2600' will work
-        // Unicode with 5 chars require brackets: '\u{1F602}'
-        lightIcon: '\u{1F602}',
-
-        lightIconStyle: {
-          marginLeft: '1px',
-        },
-      },
     },
-    
     navbar: {
-      title: 'My Site',
+      title: 'Masterworks',
       logo: {
         alt: 'Masterworks',
         src: 'img/Logo.png',
@@ -70,6 +41,7 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()}. Aurigo Software Technologies Inc.`,
     },
   },
+  
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -77,9 +49,7 @@ module.exports = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // In order to display a link to edit your documents, please change this value.
-          // Refer to Docusaurus v2 documentation for more info.
-          // editUrl: 'http://easydita.com',
+          breadcrumbs: false,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -89,12 +59,12 @@ module.exports = {
   ],
   plugins: [
     // To enable the local search functionality, uncomment the following code:
-  /* [require.resolve('@cmfcmf/docusaurus-search-local'), {
-    indexDocs: true,
-    docsRouteBasePath: '/',
-    indexDocSidebarParentCategories: 3,
-    indexPages: false,
-    language: "en"
-    }]*/
+    // [require.resolve('@cmfcmf/docusaurus-search-local'), {
+    // indexDocs: true,
+    //     docsRouteBasePath: '/',
+    //     indexDocSidebarParentCategories: 3,
+    //     indexPages: false,
+    //     language: "en"
+    // }]
   ],
 };
